479d3d682d655806db25ac7f6ad8dff083b9d341
ashushreyansh <ashushreyansh@gmail.com>
2020-09-16 19:58:27 +0530

    project_internshala

---
+++ project_solution_PHP/includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Shreyansh Chaurasia. All Rights Reserved  |  Contact Us: +91 9910321401</p>	
+        </center>
+    </div>
+</footer>
